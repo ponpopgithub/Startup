830c499fd716c849f934ec63f51e74d269e2798d
ponpopgithub <pongpop159@gmail.com>
2021-07-04 10:44:21 +0700

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup namer',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup namer'),
+        ),
+        body: const Center(
+          child: const Text('My Company'),
+        ),
+      ),
+    );
+  }
+}
